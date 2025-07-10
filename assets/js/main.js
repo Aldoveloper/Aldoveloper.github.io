2cf5bd5592bf490d8aed6284ffce613afcf00f77
Aldo <aldo.perdomo@unipamplona.edu.co>
2025-07-10 14:48:50 -0500

    new style

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Typed.js efecto
+  const typed = new Typed("#typed-text", {
+    strings: [
+      "Full Stack Developer",
+      "IoT & Visión Artificial",
+      "Integrador eCommerce",
+      "Ingeniero Mecatrónico en formación"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+  });
+});
